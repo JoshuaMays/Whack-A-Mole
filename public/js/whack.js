eda1f15c8c65ba2b6bb5370357927ba0a142443f
haydogsup <joshua.r.mays@gmail.com>
2014-10-05 01:14:28 -0500

    added highscore tracking (not on refresh), fixed game timer countdown, fixed hide and explode mole functions, and reworked game start and finish.

--- public/js/whack.js
+++ public/js/whack.js
@@ -1,64 +1,137 @@
 $(document).ready(function() {
-    var gameTime;
-    var score;
-    var popupInterval;
-    var popupTimeout;
-    
-    // FUNCTION TO DISABLE THE START BUTTON
-    var disableStartButton = function() {
-        $("#startButton").attr("disabled", "disabled");
+    var gameTimer = 30;
+    var score = 0;
+    var record = 0;
+    var moleInterval;
+    var moleTimeoutId;
+    var gameTimeout;
+    var moleTimer;
+
+    // INCREMENT THE SCORE BY ONE
+    var incrementScore = function()
+    {
+        return ++score;
     }
-    
-    // FUNCTION TO ENABLE THE START BUTTON
-    var enableStartButton = function() {
-        $("#startButton").removeAttr("disabled");
+
+    // GAME TIMER
+    var timer = function()
+    {
+        gameTimer--;
+        // DISPLAY NEW TIME LEFT
+        $("#timeClock").html(gameTimer);
     }
-    
-    // FUNCTION TO INCREMENT THE SCORE BY ONE
-    var incrementScore = function(score) {
-        score++;
-        return score;
-    }
-    
-    // FUNCTION TO GENERATE A RANDOM INT BETWEEN 1 - 9
-    var getRandomInt = function() {
+
+    // GENERATE A RANDOM INT BETWEEN 1 - 9
+    var getRandomInt = function()
+    {
         return Math.floor(Math.random() * 9) + 1;
     }
-    
-    // FUNCTION TO APPEND MOLE IMAGE TO A RANDOM BOX
-    var showRandomMole = function(boxId) {
+
+    // APPEND MOLE IMAGE TO A RANDOM BOX
+    var showRandomMole = function(boxId)
+    {
         $("<img id='mole' src='/img/bale.png' alt='bale mole'>").appendTo("#cell" + boxId).hide().fadeIn();
+        return $("#cell" + boxId);
     }
-    
-    // FUNCTION TO HIDE THE MOLE
-    var hideMole = function() {
-        $("#mole").fadeOut().remove();
-    }
-    
-    // FUNCTION TO EXPLODE THE MOLE WHEN CLICKED
-    var explodeMole = function() {
-        $("#mole").hide('explode', 1000)
-        $("#mole").remove();
-        console.log('killed him');
-        console.log(popupInterval);
-    }
-    
-    $("#startButton").click(function() {
-        // START GAME WITH SCORE AT ZERO
-        score = 0;
+
+    // HIDE THE MOLE
+    var hideMole = function()
+    {
+        $("#mole").fadeOut(500, function()
+        {
+            this.remove();
+        });
+        $("#mole").unbind("click");
         
-        // WHEN GAME STARTS, DISABLE THE START BUTTON
-        disableStartButton();
-        popupInterval = setInterval(function() {
-            
-            // GET A RANDOM NUMBER
-            var boxId = getRandomInt();
-            // SHOW MOLE IN THE RANDOM MOLE CELL
-            showRandomMole(boxId);
-            
-            $(".box").on("click","img", function() {
-                explodeMole();
-            });
-        }, 3000);
+    }
+
+    // EXPLODE THE MOLE WHEN CLICKED
+    var explodeMole = function()
+    {
+
+        $("#mole").hide('explode', 500).remove();
+        if (moleTimeoutId !== null) {
+        clearTimeout(moleTimeoutId);
+        }
+        $("#mole").unbind("click");
+    }
+
+    // START THE GAME
+    var startGame = function()
+    {
+
+        // SET SCORE TO ZERO, AND GAMETIMER TO 30
+        score = 0;
+        gameTimer = 30;
+        // INITIAL SPEED OF MOLE SPAWN
+        moleTimer = 3000;
+        // START GAMECLOCK COUNTDOWN
+        setTimeout(endGame, 30000);
+        // BEGIN MOLES
+        newMole = setInterval(spawnMole, 1000);
+    }
+    
+    var endGame = function()
+    {
+        // REPLACE HIGHSCORE WITH NEW SCORE
+        if (score > record) {
+            record = score;
+        }
+
+        // RESET GAME BACK TO START STATUS
+        $("#highScore").html(record);
+        $("#startButton").prop("disabled", false);
+        $("#mole").unbind("click");
+        $("#timeClock").hide();
+        clearInterval(moleTimeoutId);
+        clearInterval(newMole);
+        clearInterval(gameTimerId);
+    }
+
+    // SETTING TIMEOUT FOR MOLES TO APPEAR AND DISAPPEAR
+    var spawnMole = function(speed)
+    {
+        // HIDE ANY EXISTING MOLES
+        hideMole();
+        
+        // GET RANDOM NUMBER TO PLACE MOLE
+        var boxId = getRandomInt();
+        var hole = showRandomMole(boxId);
+        
+        // SET COUNTDOWN FOR MOLES NOT CLICKED TO HIDE
+        moleTimeoutId = setTimeout(function()
+        {
+            hideMole();
+        }, moleTimer);
+        
+        
+    }
+    // ATTACH CLICK LISTENER TO ANY MOLE OBJECTS
+    $("#gameBoard").on("click","#mole", function()
+    {
+        // KILL THE MOLE
+        explodeMole();
+        // INCREMENT THE SCORE
+        incrementScore();
+        // UPDATE SCOREBOARD
+        $("#scoreBoard").html(score);
+        
+        // SPEED UP MOLES
+        if(moleTimer > 100) {
+            moleTimer -= 50;
+        } else {
+            moleTimer = 100;
+        }
+        $(this).unbind("click");
     });
+
+    // CLICK LISTENER FOR START BUTTON
+    $("#startButton").click(function()
+    {
+        startGame();
+        $("#startButton").prop("disabled", true);
+        $("#timeClock").show();
+        gameTimerId = setInterval(timer, 1000);
+    });
+
 });
